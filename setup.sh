6c9c6edd1e9cc1b54cf5d0c0df1724a87e15734d
noliverio <nick@nicholasoliverio.com>
2019-02-17 22:32:19 +0000

    adding back sudo whihc is removed by vim uninstall line

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ then
     rpm -Uvh http://mirror.ghettoforge.org/distributions/gf/gf-release-latest.gf.el7.noarch.rpm
     rpm --import http://mirror.ghettoforge.org/distributions/gf/RPM-GPG-KEY-gf.el7
     yum -y remove vim-minimal vim-common vim-enhanced
-    yum -y --enablerepo=gf-plus install vim-enhanced
+    yum -y --enablerepo=gf-plus install vim-enhanced sudo
 fi
 
 # Stuff I will want on any machine
