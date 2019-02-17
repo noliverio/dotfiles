3c98caf079e858e54e324891fd277ae7960df3e3
noliverio <nick@nicholasoliverio.com>
2019-02-17 22:43:38 +0000

    removing tilda expansion to allow script to be run with sudo

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ $1 == "desktop" ]
 then
     pacman -Syu
     pacman -Sy vim
-    ln -s ~/.dotfiles/.config ~/.config
+    ln -s .dotfiles/.config .config
 elif [ $1 == "interactive_server" ]
 then
     sudo yum update -y
@@ -16,14 +16,14 @@ fi
 
 # Stuff I will want on any machine
 # Set up vim and install vim plugins.
-rm ~/.vimrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-mkdir -p ~/.vim/view
-git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
-git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/start/vim-terraform
-git clone https://github.com/rodjek/vim-puppet.git ~/.vim/pack/plugins/start/vim-puppet
+rm .vimrc
+ln -s .dotfiles/.vimrc .vimrc
+mkdir -p .vim/view
+git clone https://github.com/fatih/vim-go.git .vim/pack/plugins/start/vim-go
+git clone https://github.com/hashivim/vim-terraform.git .vim/pack/plugins/start/vim-terraform
+git clone https://github.com/rodjek/vim-puppet.git .vim/pack/plugins/start/vim-puppet
 
 # Set up bash config
-rm ~/.bashrc 
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
+rm .bashrc 
+ln -s .dotfiles/.bashrc .bashrc
 
